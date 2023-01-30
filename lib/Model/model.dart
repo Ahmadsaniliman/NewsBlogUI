d437bd1a153425bbea3bfe1a75b2098b12f57267
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-01-30 18:29:00 +0100

    another comit

--- lib/Model/model.dart
+++ lib/Model/model.dart
@@ -1,7 +1,31 @@
-import 'package:flutter/material.dart';
+class Category {
+  final String image, image1, title, updates;
 
-final List<Widget> stackedWidgets = [
-  Stack(
-    children: const [],
+  Category({
+    required this.image,
+    required this.image1,
+    required this.title,
+    required this.updates,
+  });
+}
+
+final List<Category> newCategory = [
+  Category(
+    image1: 'assets/images/Vector (34).png',
+    image: 'assets/images/image 3 (1).png',
+    title: 'Gaming',
+    updates: '+ 20 updates',
+  ),
+  Category(
+    image1: 'assets/images/Vector (32).png',
+    image: 'assets/images/image 4.png',
+    title: 'Sports',
+    updates: '+ 99 updates',
+  ),
+  Category(
+    image1: 'assets/images/Vector (33).png',
+    image: 'assets/images/image 5.png',
+    title: 'Food',
+    updates: '+ 34 updates',
   ),
 ];
--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:newsblogui/Model/model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // final Category category;
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -14,35 +16,84 @@ class HomePage extends StatelessWidget {
           ),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 50.0,
-                    width: 50.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.black45,
+              const AppBar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text('Welcome Back,  ',
+                            style: TextStyle(fontSize: 20.0)),
+                        const Text(
+                          'Liman ',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Image.asset('assets/images/image 1.png', width: 20.0),
+                      ],
                     ),
-                    // child: ,
-                  ),
-                  Container(
-                    height: 50.0,
-                    width: 50.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.black45,
+                    const SizedBox(height: 7.0),
+                    Row(
+                      children: [
+                        const Text(
+                          "we've got some exciting news for you  ",
+                          style: TextStyle(
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        Image.asset('assets/images/image 1.png', width: 20.0),
+                      ],
                     ),
-                    // child: ,
-                  ),
-                ],
+                  ],
+                ),
               ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    ...List.generate(
+                      newCategory.length,
+                      (index) => CategoryContainer(
+                        category: newCategory[index],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //
+              //
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: SizedBox(
+                  height: 100.0,
+                  width: double.infinity,
+                  //   color: Colors.green,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text('All'),
+                      Text('Popular'),
+                    ],
+                  ),
+                ),
+              ),
+              //
               Column(
                 children: [
                   Row(
-                    children: const [
-                      Text('Welcome Back,'),
-                      //  Image.asset('assets/images/image 1.png'),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Icon(Icons)
+                      const Text('Popular'),
+                      Row(
+                        children: [
+                          Image.asset('assets/images/Vector (31).png',
+                              width: 20.0),
+                        ],
+                      ),
                     ],
                   ),
                 ],
@@ -51,6 +102,100 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class CategoryContainer extends StatelessWidget {
+  const CategoryContainer({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10.0),
+      padding: const EdgeInsets.all(10.0),
+      height: 120.0,
+      width: 130.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(category.image1),
+        ),
+      ).copyWith(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 35.0,
+            width: 35.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Image.asset(category.image),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                category.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                category.updates,
+                style: const TextStyle(
+                  fontSize: 10.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppBar extends StatelessWidget {
+  const AppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: 50.0,
+          width: 50.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.black45,
+          ),
+          // child: ,
+        ),
+        Container(
+          padding: const EdgeInsets.all(10.0),
+          height: 50.0,
+          width: 50.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.black26,
+          ),
+          child: Image.asset(
+            'assets/images/Vector (30).png',
+          ),
+        ),
+      ],
     );
   }
 }
